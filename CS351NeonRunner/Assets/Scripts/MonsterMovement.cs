40661cbd3b82b526062b99173bb388f00b4d7241
KJSPAL <spallakian@gmail.com>
2025-10-24 14:10:44 -0500

    Fixed Bug where enemies get stuck

--- CS351NeonRunner/Assets/Scripts/MonsterMovement.cs
+++ CS351NeonRunner/Assets/Scripts/MonsterMovement.cs
@@ -1,62 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class MonsterMovement : MonoBehaviour
 {
     public float speed = 2f;
-    public LayerMask groundLayer;      // your ground/tilemap layers
-    public float checkDown = 0.6f;     // how far to look for ground below the front foot
-    public float frontOffset = 0.05f;  // tiny offset in front of the collider
+    public int direction = -1;
+    public LayerMask groundLayer;
+    public float checkDown = 0.6f;
+    public float frontOffset = 0.05f;
 
     Rigidbody2D rb;
     Collider2D col;
     SpriteRenderer sr;
 
-    int dir = -1; // 1 = right, -1 = left
-
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<Collider2D>();
-        sr = GetComponent<SpriteRenderer>();
-        if (sr == null) sr = GetComponentInChildren<SpriteRenderer>(); // if the sprite is a child
-
+        sr = GetComponent<SpriteRenderer>() ?? GetComponentInChildren<SpriteRenderer>();
         rb.freezeRotation = true;
-
-        // Face according to starting dir (assumes art faces RIGHT by default)
-        if (sr) sr.flipX = (dir < 0);
+        if (sr) sr.flipX = (direction < 0);
     }
 
     void FixedUpdate()
     {
-        // 1) Move horizontally
-        rb.velocity = new Vector2(dir * speed, rb.velocity.y);
+        rb.velocity = new Vector2(direction * speed, rb.velocity.y);
+        if (!GroundAhead()) Flip();//if there is no ground ahead flip direction
+    }
 
-        // 2) Find a point at the front foot
+    bool GroundAhead()//Checks if there is ground infront of the enemy to walk on
+    {
         var b = col.bounds;
         Vector2 frontFoot = new Vector2(
-            dir < 0 ? b.min.x - frontOffset : b.max.x + frontOffset,
+            direction < 0 ? b.min.x - frontOffset : b.max.x + frontOffset,
             b.min.y + 0.02f
         );
 
-        // 3) Raycast straight down to see if there’s ground ahead
-        bool groundAhead = Physics2D.Raycast(frontFoot, Vector2.down, checkDown, groundLayer);
-
-        // 4) If no ground ahead, flip direction
-        if (!groundAhead) Flip();
+        return Physics2D.Raycast(frontFoot, Vector2.down, checkDown, groundLayer);
     }
 
-    void Flip()
+    void Flip()//Flips the direction of the enemy
     {
-        dir = -dir;
-
-        // Use SpriteRenderer.flipX instead of scaling the Transform
-        if (sr) sr.flipX = (dir < 0);
-
-        // IMPORTANT: do NOT scale-flip anymore, or Animator may fight it.
-        // var s = transform.localScale;
-        // s.x = Mathf.Abs(s.x) * dir;
-        // transform.localScale = s;
+        direction = -direction;
+        if (sr) sr.flipX = (direction < 0);
     }
 }
+
